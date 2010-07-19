485fd29b74707a162d156754de8b86f70c0ffa18
Andre <andre@CR34M3-LAP.(none)>
2010-07-20 01:09:13 +0200

    [Initial commit for github] - Text transfer not complete - BibTeX issue with \pagestyle{headings} [causes stack overflow]

---
+++ removetemp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script for removal of temp files from AHCampher_thesis folder 
+rm *.aux
+rm *.toc
+rm *.log
+rm *.glo
+rm *.gls
+rm *.ind
+rm *.ilg
+rm *.lof
+rm *.out
+rm *.bbl
+rm *.idx
+rm *.*~
+rm *.blg
+rm *.lot
+rm #*#
